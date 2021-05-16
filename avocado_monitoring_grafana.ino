f34d47c49cc4bbb0421a24122ef5a2d782c86eed
Ivana <ivana.huckova@gmail.com>
2021-05-16 20:52:28 +0200

    Initial commit

---
+++ avocado_monitoring_grafana.ino
@@ -0,0 +1,212 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+#include <HCSR04.h>
+#include <LedControl.h>
+#include "config.h" // Configuration file
+
+// NTP Client
+WiFiUDP ntpUDP;
+NTPClient ntpClient(ntpUDP);
+
+// Loki & Infllux Clients
+HTTPClient httpInflux;
+HTTPClient httpLoki;
+HTTPClient httpGraphite;
+
+// Sensors
+
+DHT dht(DHT_PIN, DHT_TYPE);
+UltraSonicDistanceSensor distanceSensor(ULTRASONIC_PIN_TRIG, ULTRASONIC_PIN_ECHO);
+
+// LED
+LedControl lc=LedControl(LED_DIN,LED_CLK,LED_CS,0);
+
+// LED visualisations
+byte smile[8] = {0x3C, 0x42, 0xA5, 0x81, 0xA5, 0x99, 0x42, 0x3C};
+byte neutral[8] = {0x3C, 0x42, 0xA5, 0x81, 0xBD, 0x81, 0x42, 0x3C};
+byte sad[8] = {0x3C, 0x42, 0xA5, 0x81, 0x99, 0xA5, 0x42, 0x3C};
+byte off[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+byte err[8] = {0x00, 0x00, 0x78, 0x40, 0x70, 0x40, 0x78, 0x00};
+
+// Function to set up the connection to the WiFi AP
+void setupWiFi() {
+  Serial.print("Connecting to '");
+  Serial.print(WIFI_SSID);
+  Serial.print("' ...");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+// Function to display state of the plant on LED Matrix
+void displayState(byte character []) {
+  unsigned long currHour = ntpClient.getHours();
+  bool shouldDisplay = (currHour < 21 && currHour > 8);   // Turn off in the night
+  if (shouldDisplay)  {
+    for (int i = 0; i < 8; i++) {
+      lc.setRow(0, i, character[i]);
+    }
+  } else {
+    for (int i = 0; i < 8; i++)  {
+      lc.setRow(0, i, off[i]);
+    }
+  }
+}
+
+// Function to read soil moisture measurement
+int getSoilMoisture() {
+  // Turn the sensor ON
+	digitalWrite(MOISTURE_POWER, HIGH);  
+  // Allow power to settle
+	delay(10);        
+  // Read the digital value form sensor                   
+	int val = digitalRead(MOISTURE_PIN); 
+  // Turn the sensor OFF
+	digitalWrite(MOISTURE_POWER, LOW);   
+  // Return moisture value
+  Serial.println(val);
+	return val;         
+}
+
+// Function to read height of the plant
+double getHeight() {
+  double manualCurrentHeight = 25;
+  double height = DISTANCE_FROM_POT - distanceSensor.measureDistanceCm();
+  Serial.println(distanceSensor.measureDistanceCm());
+  if (height > manualCurrentHeight - 3 && height < manualCurrentHeight + 4) {
+    return height;
+  }
+
+  return manualCurrentHeight;  
+}
+
+// Function to read light condition
+float getLightLux() {
+  float sensor_value = analogRead(TEMP6000_PIN);  // Get raw sensor reading
+  float volts = sensor_value * TEMP6000_VCC / 1024.0;  // Convert reading to voltage
+  float amps = volts / 10000.0; // Convert to amps across 10K resistor
+  float microamps = amps * 1000000.0; // Convert amps to microamps
+  float lux = microamps * 2.0; 
+  
+  return lux;
+}
+
+// Function to check if any reading failed
+bool checkIfReadingFailed(float hum, float cels, int moist, double height, float light) {
+  if (isnan(hum) || isnan(cels) || isnan(moist) || isnan(height) || isnan(light)) {
+      // Print letter E as error
+      displayState(err);
+      Serial.println(F("Failed to read from some sensor!"));
+      return true;
+    }
+    return false;
+}
+
+// Function to create message and display current state of the plant
+String createAndDisplayState (int moist, float cels) {
+    String currentState = "";
+    if (moist) {
+      currentState = "DRY critical";
+      displayState(sad);
+    } else if( cels < 16 ) {
+      currentState = "COLD warning";
+      displayState(neutral);
+    } else if( cels > 26 ) {
+      currentState = "HOT warning";
+      displayState(neutral);
+    } else {
+      currentState = "OK info";
+      displayState(smile);
+    }
+
+    return currentState;
+}
+
+// ========== MAIN FUNCTIONS: SETUP & LOOP ========== 
+// SETUP: Function called at boot to initialize the system
+void setup() {
+
+  // Set up soil moisture pins - initially keep the moisture sensor OFF (prevents sensor rusting)
+  pinMode(MOISTURE_POWER, OUTPUT);
+	digitalWrite(MOISTURE_POWER, LOW);
+
+  // Start the serial output at 115,200 baud 
+  Serial.begin(115200);
+
+  // Connect to WiFi
+  setupWiFi();
+
+  // Initialize a NTPClient to get time
+  ntpClient.begin();
+
+  // Start the DHT sensor
+  dht.begin();
+
+  // Start LED Matrix
+  lc.shutdown(0,false);       
+  lc.setIntensity(0,0.0000001);      //Adjust the brightness maximum is 15
+  lc.clearDisplay(0);    
+}
+
+// LOOP: Function called in a loop to read from sensors and send them do databases
+void loop() {
+  // Reconnect to WiFi if required
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.disconnect();
+    yield();
+    setupWiFi();
+  }
+
+  // Update time via NTP if required
+  while (!ntpClient.update()) {
+    yield();
+    ntpClient.forceUpdate();
+  }
+
+  // Get current timestamp
+  unsigned long ts = ntpClient.getEpochTime();
+
+  // Read humidity
+  float hum = dht.readHumidity();
+
+  // Read temperature as Celsius (the default)
+  float cels = dht.readTemperature();
+  Serial.println(cels);
+
+  // Read soil moisture (DRY: 1, WET: 0)
+  int moist = getSoilMoisture();
+
+  // Read height of plant
+  double height = getHeight();
+
+  //Read the light in lux
+  float light = getLightLux();
+
+  // Check if any reads failed and return early
+  if (checkIfReadingFailed(hum, cels, moist, height, light)) {
+    return;
+  }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(cels, hum, false);
+
+  // Convert data to state of the plant
+  String message = createAndDisplayState(moist, cels);
+
+  // Submit data
+
+  // wait INTERVAL seconds, then do it again
+  delay(INTERVAL * 1000);
+}
---
+++ config.h
@@ -0,0 +1,24 @@
+// WiFi
+#define WIFI_SSID     "wifi_name" // Add wifi name
+#define WIFI_PASSWORD "wifi_password"  // Add wifi passowrd
+
+#define ID "avocado" // Add unique name for this sensor
+#define INTERVAL 5  // Add interval (e.g. 1 min)
+
+#define DHT_PIN 5    // Which pin is DHT 22 connected to
+#define DHT_TYPE DHT11 // Type DHT 22
+
+#define MOISTURE_POWER 33 // Which pin is soil moisture sensor connected to as a source of power (prevents fast corrosioon)
+#define MOISTURE_PIN 27 // Which pin is soil moisture sensor connected to
+#define DISTANCE_FROM_POT 36.40 // Calculated so we get the height of plant frm the soil
+
+#define ULTRASONIC_PIN_TRIG 14 // Which pin is HC-SR04's trig connected to
+#define ULTRASONIC_PIN_ECHO 12 // Which pin is HC-SR04's echo connected to
+
+#define TEMP6000_PIN 26 // Light sensor pin
+#define TEMP6000_VCC 5.0 // Light used voltage
+
+// LED Matrix
+#define LED_DIN 18
+#define LED_CS 19
+#define LED_CLK 21
